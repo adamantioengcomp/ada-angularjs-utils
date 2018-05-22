cb7ae832c31bd8662b42bde8648741b1d2d0ad48
william <william@adamantio.com.br>
2018-05-22 17:08:36 -0300

    alterado o ada-history para incluir o evento .onSuccess, pois o  foi deprecated

--- ada-history/ada-history.js
+++ ada-history/ada-history.js
@@ -1,73 +1,40 @@
-require('angular').module('history',[])
-	.value('history',[])
-	.value('status',{current:-1,goingBack:false,goingForward:false})
-	.factory('History',['history','$location','status',function(history,$location,status){
+require('angular').module('history', [])
+	.value('history', [])
+	.value('status', { current: -1, goingBack: false, goingForward: false })
+	.factory('History', ['history', '$location', 'status', function (history, $location, status) {
 
-			return{
+		return {
 
-				/**
-				 * Navigate back to a specific page
-				 * @param {Object} params optional parameter that makes possible to
-				 *				   change/add params of the target url before goes back.
-				 *				   must be displayed as a key/value pair
-				 */
-				backTo : function(url,params){
-					status.goingBack = true;
-					for (var i in history){
-						var index = history[i].indexOf("?");
-						if (history[i].slice(0,(index > -1) ? index : undefined) == url){
-							status.current = i;
-							var prevUrl = history[i];
-							//var prevUrl = url;
-							//history.slice(0,Number(i)+1);
-							break;
-						}
+			/**
+			 * Navigate back to a specific page
+			 * @param {Object} params optional parameter that makes possible to
+			 *				   change/add params of the target url before goes back.
+			 *				   must be displayed as a key/value pair
+			 */
+			backTo: function (url, params) {
+				status.goingBack = true;
+				for (var i in history) {
+					var index = history[i].indexOf("?");
+					if (history[i].slice(0, (index > -1) ? index : undefined) == url) {
+						status.current = i;
+						var prevUrl = history[i];
+						//var prevUrl = url;
+						//history.slice(0,Number(i)+1);
+						break;
 					}
+				}
 
-					if (prevUrl){
-						for (i in params){
-							var index = prevUrl.indexOf("?"+i+"=");
-							if (index < 0) index = prevUrl.indexOf("&"+i+"=");
-							if (index > -1){
-								var tmp = prevUrl.substr(index+1);
-								if (tmp.indexOf("&") > 0){
-									tmp = tmp.substr(0,tmp.indexOf("&"));
-								}							
-								prevUrl = prevUrl.replace(tmp,i+"="+params[i]);
-							}else{
-								if (prevUrl.indexOf("?") > -1)
-									prevUrl += "&";
-								else
-									prevUrl += "?";
-								prevUrl += (i + "=" + params[i]);
+				if (prevUrl) {
+					for (i in params) {
+						var index = prevUrl.indexOf("?" + i + "=");
+						if (index < 0) index = prevUrl.indexOf("&" + i + "=");
+						if (index > -1) {
+							var tmp = prevUrl.substr(index + 1);
+							if (tmp.indexOf("&") > 0) {
+								tmp = tmp.substr(0, tmp.indexOf("&"));
 							}
-						}
-					}
-
-        			window.location  = "#" + prevUrl;
-				},
-
-				/**
-				 * Navigate to the previous page
-				 * @param {Object} params optional parameter that makes possible to
-				 *				   change/add params of the target url before goes back.
-				 *				   must be displayed as a key/value pair
-				 */
-				back : function(params){
-					status.goingBack = true;
-					(status.current > 0) ? status.current-- : status.current = 0;
-					var prevUrl = history.length > 0 ? history[status.current] : "/";
-
-					for (i in params){
-						var index = prevUrl.indexOf("?"+i+"=");
-						if (index < 0) index = prevUrl.indexOf("&"+i+"=");
-						if (index > -1){
-							var tmp = prevUrl.substr(index+1);
-							if (tmp.indexOf("&") > 0){
-								tmp = tmp.substr(0,tmp.indexOf("&"));
-							}							
-							prevUrl = prevUrl.replace(tmp,i+"="+params[i]);
-						}else{
+							prevUrl = prevUrl.replace(tmp, i + "=" + params[i]);
+						} else {
 							if (prevUrl.indexOf("?") > -1)
 								prevUrl += "&";
 							else
@@ -75,49 +42,86 @@ require('angular').module('history',[])
 							prevUrl += (i + "=" + params[i]);
 						}
 					}
-
-        			window.location  = "#" + prevUrl;
-				},
-
-				/**
-				 * Navigate to the next page
-				 */
-				forward : function(){
-					status.goingForward = true;
-					(status.current > 0) && (status.current < (history.length-1)) ? status.current++ : status.current = 0;
-					var nextUrl = history.length > 0 ? history[status.current] : "/";
-        			$location.path(nextUrl);
-				},
-
-				/**
-				 * Clear the history
-				 */
-				clear : function(){
-					status.current = 0;
-					history.splice(0,history.length);
 				}
-			};
 
-		}])
-	.run(['$rootScope','history','$location','status',function($rootScope, history,$location,status){
-		
-		var changeEvent = function(){
-			if (!status.goingBack && !status.goingForward){				
+				window.location = "#" + prevUrl;
+			},
+
+			/**
+			 * Navigate to the previous page
+			 * @param {Object} params optional parameter that makes possible to
+			 *				   change/add params of the target url before goes back.
+			 *				   must be displayed as a key/value pair
+			 */
+			back: function (params) {
+				status.goingBack = true;
+				(status.current > 0) ? status.current-- : status.current = 0;
+				var prevUrl = history.length > 0 ? history[status.current] : "/";
+
+				for (i in params) {
+					var index = prevUrl.indexOf("?" + i + "=");
+					if (index < 0) index = prevUrl.indexOf("&" + i + "=");
+					if (index > -1) {
+						var tmp = prevUrl.substr(index + 1);
+						if (tmp.indexOf("&") > 0) {
+							tmp = tmp.substr(0, tmp.indexOf("&"));
+						}
+						prevUrl = prevUrl.replace(tmp, i + "=" + params[i]);
+					} else {
+						if (prevUrl.indexOf("?") > -1)
+							prevUrl += "&";
+						else
+							prevUrl += "?";
+						prevUrl += (i + "=" + params[i]);
+					}
+				}
+
+				window.location = "#" + prevUrl;
+			},
+
+			/**
+			 * Navigate to the next page
+			 */
+			forward: function () {
+				status.goingForward = true;
+				(status.current > 0) && (status.current < (history.length - 1)) ? status.current++ : status.current = 0;
+				var nextUrl = history.length > 0 ? history[status.current] : "/";
+				$location.path(nextUrl);
+			},
+
+			/**
+			 * Clear the history
+			 */
+			clear: function () {
+				status.current = 0;
+				history.splice(0, history.length);
+			}
+		};
+
+	}])
+	.run(['$rootScope', 'history', '$location', 'status', '$transitions',function ($rootScope, history, $location, status,$transitions) {
+
+		var changeEvent = function () {
+			if (!status.goingBack && !status.goingForward) {
 				//history.push($location.$$url);
 				status.current++;
 				history[status.current] = $location.$$url;
-			}else{
+			} else {
 				status.goingBack = false;
 				status.goingForward = false;
 			}
 		};
 
-		$rootScope.$on('$routeChangeSuccess', function() {
+		$rootScope.$on('$routeChangeSuccess', function () {
 			changeEvent();
-	    });
+		});
 
-	    $rootScope.$on('$stateChangeSuccess', function() {
+		$rootScope.$on('$stateChangeSuccess', function () {
 			changeEvent();
-	    });
+		});
+
+		$transitions.onSuccess({}, function () {
+			changeEvent();
+		});
 
 	}]);
