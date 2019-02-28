34c6d45f1161f41ee736f0ceed16dc9941621379
william <william@adamantio.com.br>
2019-02-28 14:09:59 -0300

    alterado ada-backbutton para incluir multiplos callbacks por rota e possibilidade de chamada em cadeia

--- ada-back-button/ada-back-button.js
+++ ada-back-button/ada-back-button.js
@@ -27,27 +27,77 @@ backButton = require('angular').module('ada-back-button',[])
 				/**
 				 * Add a callback for the location
 				 */ 
-				addEvent : function(location, callback){
-					callbacks[location] = callback;
+				addEventForLocation : function(location, callback){
+					if(!callbacks[location]){
+						callbacks[location] = [callback];
+						return;
+					}
+
+					//Verifica se o callback já foi registrado para o local
+					let exists = callbacks[location].find((cb)=>{
+						return(cb.toString() == callback.toString())
+					});
+					//Se já existir, registra por cima do existente (para atualizar o escopo da função)
+					if(exists){
+						let cbs = callbacks[location];
+						cbs[cbs.indexOf(exists)] = callback;
+					}else{
+						//Se não existe, registra o novo callback
+						callbacks[location].push(callback);
+					}					
 				},
 
 				/**
 				 * Add a callback for the current location
 				 */ 
 				addEvent : function(callback){
-					callbacks[$location.path()] = callback;
+					this.addEventForLocation($location.path(),callback);
+				},
+
+				/**
+				 * Configura a função de callback, incluindo nera o parametro next, apontando para o 
+				 * proximo callback na cadeia de callbacks
+				 */
+				executeNext(cb,route,index){
+					var self = this;
+					if (callbacks[route]){						
+						let availableCallbacks = callbacks[route];
+						let nextIndex = index-1;
+						let nextFn = function(){ /* do nothing */};
+						if(index > 0){							
+							nextFn = availableCallbacks[nextIndex];
+						}else if(index == 0){
+							if (defaultCallback && defaultCallback[0]){
+								nextFn = defaultCallback[0];
+							}	
+						}
+						
+						cb(function(){
+							self.executeNext(nextFn,route,nextIndex);
+						})
+					}
 				},
 
 				/**
 				 * Executes the back buton callback programatically
 				 */
 				executeBackButtonCallback : function(){
+					debugger;
+					//Primeiro executa os callbacks globais, na ordem em que foram registrados
+					//Mas se algum retornar 'true', interrompe a corrente
 					for (var i in globalCallbacks){
 						if (globalCallbacks[i]()) return;
 					}
 
-					if (callbacks[$location.path()]){
-						callbacks[$location.path()]();
+					//Busca os callbacks registrados para a rota
+					if (callbacks[$location.path()] && (callbacks[$location.path()].length > 0)){						
+						let availableCallbacks = callbacks[$location.path()];
+
+						//O callback a ser executado é o registrado por último
+						let index = (availableCallbacks.length -1);
+						var callbackToExecute = availableCallbacks[index];
+
+						this.executeNext(callbackToExecute,$location.path(),index);
 					}else{
 						if (defaultCallback && defaultCallback[0])
 							defaultCallback[0]();
